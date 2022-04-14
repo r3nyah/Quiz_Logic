bd5742e357f203ec9c1bfc84c3ed05910e14a898
Arian Febrian <88012298+r3nyah@users.noreply.github.com>
2022-04-14 11:27:04 +0700

    Delete Quiz directory

--- Quiz/No3.java
+++
@@ -1,9 +0,0 @@
-public class No3 {
-    public static void mian(String[] args){
-        int i = 0;
-        while (i<=5){
-            System.out.println(i+1);
-            i++;
-        }
-    }
-}
