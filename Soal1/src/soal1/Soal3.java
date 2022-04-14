2f99e183310f686ff48fa00576dcd16bb5e6216d
Arian Febrian <88012298+r3nyah@users.noreply.github.com>
2022-04-14 11:33:58 +0700

    Add files via upload

---
+++ Soal1/src/soal1/Soal3.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package soal1;
+
+/**
+ *
+ * @author Arian
+ */
+public class Soal3 {
+    public static void main(String[] args){
+        int n = 5;
+        int hasil = 0;
+        
+        for(int i=1;i<=n;i++)
+        hasil +=i;
+            System.out.println("Hasil = "+hasil);
+        
+        
+    }
+}
